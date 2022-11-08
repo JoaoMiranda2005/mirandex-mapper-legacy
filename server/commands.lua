ed6539e18381a4b89aaf0c4ff765ed8ad28eb62e
João Miranda <88510677+JoaoMiranda2005@users.noreply.github.com>
2022-11-08 19:07:53 +0000

    Update commands.lua

--- server/commands.lua
+++ server/commands.lua
@@ -1,20 +1,3 @@
---[[TriggerEvent('es:addGroupCommand', 'mapper', "admin", function(source, args, user)
-	TriggerClientEvent('es_mapper:toggle', source)
-end, function(source, args, user)
-	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
-end, {help = 'Edit Map'})]]--
-
-
---[[RegisterCommand('mapper', function(source, args, xPlayer)
-	local xPlayer = xPlayer
-	if xPlayer.getGroup() ~= 'admin' then
-		TriggerClientEvent('es_mapper:toggle', source)
-	else
-		ESX.ShowNotification('Não tens permissões!', 5000, info)
-		-- https://docs.esx-framework.org/legacy/Client/functions/shownotification
-	end
-end)]]--
-
 ESX = exports['es_extended']:getSharedObject()
 
 RegisterCommand('mapper', function(source, args)
